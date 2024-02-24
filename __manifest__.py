4a901579da87237b59bbbb5ed2f7f8d3b31783bd
miguelsubiran <160909900+miguelsubiran@users.noreply.github.com>
2024-02-24 13:22:55 +0100

    cambios

---
+++ __manifest__.py
@@ -0,0 +1,16 @@
+# Copyright 202 Navarmedia - Miguel Subiran
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+{
+    "name": "Contratos Electricidad y Gas - CRM",
+    "version": "1.0.0",
+    "category": "Contratos Electricidad",
+    "licence": "AGPL-3",
+    "author": "Navarmedia, Odoo Community Association (OCA)",
+    "website": "https://github.cm/OCA/electricity_contract",
+    "depends": ["base", "account"],
+    "development_status": "pruebas",
+    "data": [
+        "views/res_partner.xml"
+    ]
+}
