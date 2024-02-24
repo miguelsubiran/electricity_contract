ce444cf7a112fce884d6aacf7a82798e6f67d203
miguelsubiran <160909900+miguelsubiran@users.noreply.github.com>
2024-02-24 12:20:52 +0100

    Cambios

---
+++ models/res_partner.py
@@ -0,0 +1,7 @@
+# Copyright 2023 Miguel Subiran <miguel@navarmedia.com>
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+from odoo import fields, models
+
+class ResPartner(models.Model):
+    _inherit = "res.partner"
