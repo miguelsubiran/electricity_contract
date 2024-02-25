077c8178e87cd10c9d466a15f182c442df3e29b7
miguelsubiran <160909900+miguelsubiran@users.noreply.github.com>
2024-02-25 18:06:00 +0100

    c

--- models/electricity_contract.py
+++ models/electricity_contract.py
@@ -1,10 +1,11 @@
 # Copyright 2023 Miguel Subiran <miguel@navarmedia.com>
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
-from odoo import fields, models
+from odoo import models, fields
 
 class Contract(models.Model):
     _name = 'electricity.contract.contract.electricity'
     _description = 'Contratos'
 
-    nombrecontrato = fields.Char()
+    nombre_contrato = fields.char()
+    
