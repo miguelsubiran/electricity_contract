2d425858ceb5d6d57a6b4e895bd0cc2a52bb03f2
miguelsubiran <160909900+miguelsubiran@users.noreply.github.com>
2024-02-25 17:42:55 +0100

    c

--- models/electricity_contract.py
+++ models/electricity_contract.py
@@ -7,3 +7,5 @@ class Contract(models.Model):
     _name = 'electricity.contract.contract.electricity'
     _description = "Contratos"
 
+    nombre_contrato = fields.chr(string='Contrato', required=True)
+    partner_name = fields.chr(string='Cliente', required=True)
