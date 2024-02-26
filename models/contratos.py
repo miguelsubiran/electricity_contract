aed34b4d70623cee7ad921fe3aaffcc0e254dc25
miguelsubiran <160909900+miguelsubiran@users.noreply.github.com>
2024-02-26 09:00:06 +0100

    c

--- models/contratos.py
+++ models/contratos.py
@@ -4,4 +4,6 @@ class contratos(models.Model):
     _name = 'contratos.electricos'
     _description = 'Contratos'
 
-    nombre_contrato = fields.Char()
+    nombre_contrato = fields.Char(string='Contrato', required=True)
+    tipo_contrato = fields.one2many(string='Tipo de contrato', required=True)
+    
