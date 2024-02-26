77092e9f4e87fedfc804d7261e8dae06e86c0d1a
miguelsubiran <160909900+miguelsubiran@users.noreply.github.com>
2024-02-26 11:39:56 +0100

    c

--- models/contratos.py
+++ models/contratos.py
@@ -5,10 +5,10 @@ class contratos(models.Model):
     _description = 'Contratos'
 
     nombre_contrato = fields.Char(string='Contrato', required=True)
-    tipo_contrato_ids = fields.One2many('tipo.contrato', 'tipocontrato_id', string='Tipo de contrato')
+    tipo_contrato_ids = fields.Many2one('tipo.contrato', 'tipocontrato_id', string='Tipo de contrato')
 
 class TipoContrato(models.Model):
     _name = 'tipo.contrato'
     _description = 'Tipos de Contrato'
 
-    tipocontrato_id = fields.Many2one('contratos.electricos', string='Tipo de Contrato')
+    tipocontrato_id = fields.Char(string='Tipo de Contrato', required=True)
