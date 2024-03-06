d74cbed7aa419ceeb9c3070e1f998ed5f5ee0001
miguelsubiran <160909900+miguelsubiran@users.noreply.github.com>
2024-03-06 13:28:19 +0100

    c

--- models/contratos.py
+++ models/contratos.py
@@ -9,7 +9,7 @@ class contratos(models.Model):
     fecha_alta=fields.Date(string='F.alta')
     fecha_vencimiento=fields.Date(string='F.Vto.')
     estado_id=fields.Many2one('estado.contrato',string='Estado')
-    #tipo_contrato_id = fields.Many2one('tipo.contrato', string='Tipo de contrato')
+    tipo_contrato_id = fields.Many2one('tipo.contrato', string='Tipo de contrato')
     #cliente_id = fields.Many2one('res.partner',string='Cliente')
     #comercial_id = fields.Many2one('res.user',string='Comercial')
     #cups_id=fields.Many2one('cups.contrato',string='CUPS')
